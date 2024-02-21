1e527a30eeaa8c19fcee44dd3861d40ebe8cfd90
Hon Nguyen <hon.tnguyen294@gmail.com>
2024-02-20 21:57:08 -0800

    fixed Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import {Link} from 'react-router-dom'
 const Navbar = ()=>{
    return(
       <div>
-         <nLinkv>
+         <nav>
            <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/register">Register</Link></li>
-            <li><Link to="/profile">EmLinkil Dropdown</Link></li>
+            <li><Link to="/profile">Email Dropdown</Link></li>
             <li><Link to="/logout">Logout</Link></li>
            </ul>
-         </nLinkv>
+         </nav>
       </div>
    )
 
