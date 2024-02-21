1c28796b393003e6ee2628b0bba3e6f4b99b146d
Hon Nguyen <hon.tnguyen294@gmail.com>
2024-02-21 06:00:09 -0800

    finished off Cart displaying

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,9 +2,56 @@
 const Cart = ({cart})=>{
 
    console.log('cart in Cart:::',cart)
+   const onDecrease = ()=>{
+
+   }
+   const onIncrease = ()=>{
+      
+   }
    return (
       <div className="cart">
-         <h1>Cart Page...</h1>
+         <div className="cart-center"> <table>
+               <thead>
+                  <tr>
+                     <th>id</th>
+                     <th>Preview</th>
+                     <th>Title</th><th>Discount</th>
+                     <th>Price</th>
+                     <th>Quantity</th>
+                     <th>SubTotal</th>
+                     <th>Action</th>
+                  </tr>
+               </thead>
+               <tbody>
+               {cart.length>0&& cart.map((item,index)=>(
+                  <tr key={index}>
+                     <td>{item.id}</td>
+                     <td><img src={item.thumbnail} alt="logo" id="photo"/></td>
+                     <td width="250px">{item.title}</td>
+                     <td>{item.discountPercentage}%</td>
+                     <td>${item.price}</td>
+                     <td>
+                        <div>
+                           <button id="q-decrease" onClick={onDecrease}>-</button>
+                           <input type="text" id="quantity" name="quantity"  value={item.quantity}
+                           />
+                           <button id="q-increase" onClick={onIncrease}>+</button>
+                        </div>
+                     </td>
+                     <td>$$$</td>
+                     <td>
+                     <button id="delete">
+                           <span className="material-symbols-outlined">
+                              delete
+                           </span> Remove All
+                           </button>
+                     </td>
+                  </tr>
+            ))}
+               
+               </tbody>
+            </table></div>
+         <div className="payment-area">Payment</div>
       </div>
    )
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,9 +5,11 @@ import {useState} from 'react'
 const ProductList = ({ products, callback }) => {
 
    const [cartProducts, setCartProducts]=useState([])
+   const [message,setMessage]=useState('')
    
    const handleAdd =(item)=>{     
-      
+      setMessage("Item added.")
+      setTimeout(()=>setMessage(''),3000)
       let tempCart=[...cartProducts]
 
       let existingItemIndex = tempCart.findIndex((elem)=>elem.id===item.id)     
@@ -15,7 +17,7 @@ const ProductList = ({ products, callback }) => {
       if(existingItemIndex===-1){
          tempCart.push({...item,quantity:1})
          setCartProducts(tempCart)
-         
+
          // console.log('cart in main')
          // console.log(tempCart)
       } else {
@@ -33,9 +35,14 @@ const ProductList = ({ products, callback }) => {
    }
 
    return (
-      <div className="products-container">
+      <div className="top">
+      {message && <p className='success'>{message}</p>}
+  <div className="products-container">
+       
          <div className="left"></div>
+       
          <div className="products">
+      
             {products &&
                products.map((item) => {
                   let {
@@ -49,8 +56,11 @@ const ProductList = ({ products, callback }) => {
                      thumbnail,
                   } = item;
                   return (
+                    
                      <li key={id}>
+                   
                         <div className="product-card">
+                       
                         <p>
                               <img src={thumbnail} alt="product" id="product-img"/>
                            </p>
@@ -74,6 +84,55 @@ const ProductList = ({ products, callback }) => {
          </div>
          <div className="right"></div>
       </div>
+<div className="products-container">
+       
+       <div className="left"></div>
+     
+       <div className="products">
+    
+          {products &&
+             products.map((item) => {
+                let {
+                   id,
+                   title,
+                   description,
+                   price,
+                   discountPercentage,
+                   stock,
+                   category,
+                   thumbnail,
+                } = item;
+                return (
+                  
+                   <li key={id}>
+                 
+                      <div className="product-card">
+                     
+                      <p>
+                            <img src={thumbnail} alt="product" id="product-img"/>
+                         </p>
+                         <p>id: {id}</p>
+                         <p>{title}</p>
+                         <h3>Price: ${price}</h3>
+                         <p>Discount Percent: {discountPercentage}%</p>
+                         <p><em>Details: {description}</em></p>
+                         <p>In stock: {stock}</p>
+                         <p><em>Category:</em> <span id="category">{category}</span></p>
+                         <div id="btn-add">
+                            <button 
+                            onClick={()=>{handleAdd(item)}}
+                            id="click-add">Add to Cart</button>
+                         </div>
+                        
+                      </div>
+                   </li>
+                );
+             })}
+       </div>
+       <div className="right"></div>
+    </div>
+      </div>
+    
    );
 };
 export default ProductList;
