e5a05ec6fc71f9daf329f3605a44495e881dacee
Hon Nguyen <hon.tnguyen294@gmail.com>
2024-02-20 21:29:28 -0800

    fixed: Navbar url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,15 @@
-
+import {Link} from 'react-router-dom'
 const Navbar = ()=>{
-
-
    return(
       <div>
-         <nav>
+         <nLinkv>
            <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="register">Register</a></li>
-            <li><a href="profile">Email Dropdown</a></li>
-            <li><a href="logout">Logout</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/profile">EmLinkil Dropdown</Link></li>
+            <li><Link to="/logout">Logout</Link></li>
            </ul>
-         </nav>
+         </nLinkv>
       </div>
    )
 
